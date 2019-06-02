de375d88f6f7fa2206edf081a203fc272909bffd
Capwell <admin@axerius.net>
2019-06-02 17:27:00 +0100

    Fixed Correct Path Includes

--- addons/EC_Factions/config.cpp
+++ addons/EC_Factions/config.cpp
@@ -2,9 +2,9 @@
 #define weap_xx(a,b) class _xx_##a {weapon = ##a; count = b;}
 #define item_xx(a,b) class _xx_##a {name = a; count = b;}
 
-#include "CfgPatches.hpp"
-#include "CfgFactionClasses.hpp"
-#include "CfgVehicleClasses.hpp"
-#include "CfgVehicles.hpp"
-#include "CfgGroups.hpp"
-#include "crates.hpp"
+#include "includes\CfgPatches.hpp"
+#include "includes\CfgFactionClasses.hpp"
+#include "includes\CfgVehicleClasses.hpp"
+#include "includes\CfgVehicles.hpp"
+#include "includes\CfgGroups.hpp"
+#include "includes\crates.hpp"
