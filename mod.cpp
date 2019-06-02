ecf265b04950ad24408edb223d3aee8a49656a83
Capwell <admin@axerius.net>
2019-06-02 19:29:01 +0100

    Created mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,28 @@
+/// Name of your mod
+name = "Splendid mod";
+/// Picture displayed from the Expansions menu
+picture = "\Samples_F\Data_01\Images\picture.paa";
+/// Display next to the item added by the mod
+logoSmall = "\Samples_F\Data_01\Logos\logo_small.paa";
+/// Logo displayed in the main menu
+logo = "\Samples_F\Data_01\Logos\logo.paa";
+/// When the mouse is over, in the main menu
+logoOver = "\Samples_F\Data_01\Logos\logoOver.paa";
+/// Website URL, that can accessed from the extansion menu
+action = "https://community.bistudio.com/wiki/Mod_Presentation";
+/// Tool tip displayed when the mouse is left over, in the main menu
+tooltipOwned = "It's yours!";
+/// Color used for DLC stripes and backgrounds
+dlcColor[] = 
+{
+	0.23, // Red
+	0.39, // Green
+	0.30, // Blue
+	1 // Alpha
+};
+/// Overview text, displayed from the extansion menu
+overview = "Unleash your creativity! Here goes the long description of your mod.";
+/// Hide the extansion name 
+hideName = 0;
+/// Hide the extansion menu
+hidePicture = 0;
