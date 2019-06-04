393fe40097264eacf6222cf24790a5a16173c7c4
Capwell <admin@axerius.net>
2019-06-04 17:02:25 +0100

    Created EC_Data

---
+++ addons/EC_Data/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches {
+    class EC_Data {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {};
+        author = "Capwell";
+        authors[] = {"Capwell"};
+    };
+};
