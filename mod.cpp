caca8a56a383d5f6027e87218a8fe3829d2cda4c
Capwell <admin@axerius.net>
2019-06-04 17:01:52 +0100

    Updated Mod

--- mod.cpp
+++ mod.cpp
@@ -1,27 +1,27 @@
 /// Name of your mod
-name = "Splendid mod";
+name = "Epsilon Community Internal Modifications";
 /// Picture displayed from the Expansions menu
-picture = "\Samples_F\Data_01\Images\picture.paa";
+picture = "EC_Data\Images\picture.paa";
 /// Display next to the item added by the mod
-logoSmall = "\Samples_F\Data_01\Logos\logo_small.paa";
+logoSmall = "EC_Data\Images\logo_small.paa";
 /// Logo displayed in the main menu
-logo = "\Samples_F\Data_01\Logos\logo.paa";
+logo = "EC_Data\Images\logo.paa";
 /// When the mouse is over, in the main menu
-logoOver = "\Samples_F\Data_01\Logos\logoOver.paa";
+logoOver = "EC_Data\Images\logoOver.paa";
 /// Website URL, that can accessed from the extansion menu
-action = "https://community.bistudio.com/wiki/Mod_Presentation";
+action = "https://discord.epsilon.community";
 /// Tool tip displayed when the mouse is left over, in the main menu
-tooltipOwned = "It's yours!";
+tooltipOwned = "Join via our Discord.";
 /// Color used for DLC stripes and backgrounds
 dlcColor[] = 
 {
-	0.23, // Red
-	0.39, // Green
-	0.30, // Blue
+	0.38, // Red
+	0.68, // Green
+	0.79, // Blue
 	1 // Alpha
 };
 /// Overview text, displayed from the extansion menu
-overview = "Unleash your creativity! Here goes the long description of your mod.";
+overview = "Epsilon Community is a semi-serious, modification intensive, and player-first community. Utilising quality scripting along with excellent Zeus co-ordination, allowing for the correct pace for both the Player and Zeus. Striving to achieve excellent team play and allow for the best combat experience, with realistic tactics and communication.";
 /// Hide the extansion name 
 hideName = 0;
 /// Hide the extansion menu
